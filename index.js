21a4120831d84ed346d216727ff47138dd0878f2
Amine Haddou <haddouamine2002@gmail.com>
2021-12-04 11:14:23 +0300

    Bot fonctionnel avec log + commandes fun

--- index.js
+++ index.js
@@ -3,14 +3,35 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-exports.c = client;
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_MESSAGE_REACTIONS"] });
 
+
+exports.c = client;
+exports.p = ".";
 // When the client is ready, run this code (only once)
+
 client.once('ready', () => {
 	console.log(`Ready! Logged in as ${client.user.tag}`);
-        client.user.setActivity('twitch.tv/hazzuna_', { type: 'WATCHING' });
+        const nombreMembre = client.guilds.cache.get("841455115445600317").memberCount;
+        var affichage = 0;
+        setInterval(()=>{
+                if(affichage = 0){
+                        client.user.setActivity(`${nombreMembre} membres`, { type: 'WATCHING' });
+                        affichage = 1;
+                } else {
+                        client.user.setActivity('twitch.tv/hazzuna_', { type: 'WATCHING' });
+                        affichage = 0;
+                }
+        },2000)
 });
+
+//code
+
+client.on("messageCreate", require("./fun/ping").fun);
+client.on("messageCreate", require("./fun/pote").pote);
+client.on("messageDelete", require("./log/messageSupp").log);
+client.on("messageUpdate", require("./log/messageModif").log2);
+
 
 // Login to Discord with your client's token
 client.login(token);
