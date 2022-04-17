d73d5fbd8e288175fcb19fb46f7ec80b9fbba01d
Amine Haddou <haddouamine2002@gmail.com>
2022-04-17 19:15:05 +0300

    Ajoute de l'option questionnement + qlq corrections dans DiscuDm et index

---
+++ fun/questionningBot.js
@@ -0,0 +1,19 @@
+const index = require('../index.js');
+const { MessageEmbed } = require('discord.js');
+
+exports.questionning = message =>{
+    if(message.author.bot)return;
+    if(!(message.content.startsWith(index.p+"q"))) return;
+    const args = message.content.split(' ');
+    args.shift();
+    const answer= ["Oui.", "Probablement.", "Non.", "C'est certain.", "Impossible.", "C'est certainement vrai.", "Je ne sais pas...", "Google existe.", "J'aurais tendance à dire non.", "La fameuse question a 1M."]
+    const messAnswer = new MessageEmbed()
+        .setColor('#fab534')
+        .setAuthor(`${message.author.username}#${message.author.tag}`, `${message.author.displayAvatarURL()}`)
+        .setThumbnail()
+        .addField(`**Question** :`, `${args.join(" ")}`)
+        .addField(`**Réponse** : `, `${answer[Math.floor(Math.random() * (answer.length) )]}`)
+        .setFooter(`Vous êtes beaux`, 'https://i.postimg.cc/nhZbcy3d/aigle.png')
+        .setTimestamp();
+    message.channel.send({ embeds: [messAnswer] });
+}
